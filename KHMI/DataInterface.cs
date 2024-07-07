211033e1aa6ccbdc457af78ac9ba582c67bf1b48
synth <wavydave2000@gmail.com>
2024-07-07 03:35:13 -0400

    add datainterface

---
+++ KHMI/DataInterface.cs
@@ -0,0 +1,65 @@
+﻿namespace KHMI
+{
+
+    // class to wrap data management of commonly used variables
+    public class DataInterface
+    {
+        private MemoryInterface memInterface;
+
+        public DataInterface(MemoryInterface mi)
+        {
+            if (mi != null)
+            {
+                memInterface = mi;
+            }
+        }
+
+        public int WorldID
+        {
+            get
+            {
+                return memInterface.readInt(memInterface.nameToAddress("WorldID"));
+            }
+            set
+            {
+                memInterface.writeInt(memInterface.nameToAddress("WorldID"), value);
+            }
+        }
+
+        public int RoomID
+        {
+            get
+            {
+                return memInterface.readInt(memInterface.nameToAddress("RoomID"));
+            }
+            set
+            {
+                memInterface.writeInt(memInterface.nameToAddress("RoomID"), value);
+            }
+        }
+
+        public int SceneID
+        {
+            get
+            {
+                return memInterface.readInt(memInterface.nameToAddress("SceneID"));
+            }
+            set
+            {
+                memInterface.writeInt(memInterface.nameToAddress("SceneID"), value);
+            }
+        }
+
+        public int WarpID
+        {
+            get
+            {
+                return memInterface.readInt(memInterface.nameToAddress("WarpID"));
+            }
+            set
+            {
+                memInterface.writeInt(memInterface.nameToAddress("WarpID"), value);
+            }
+        }
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using KHMI;
 
-// simple tester program to showcase the dll
+// simple tester program to showcase the dll - inserts a jump table in the main loop with code that sets world id to 1 (destiny islands) and room id to 0 (seashore)
 
 
 MemoryInterface mi = new MemoryInterface(Provider.EPIC, "1.0.0.9"); // creates memory interface for EPIC version 1.0.0.9. affects memory offsets
@@ -20,28 +20,11 @@ do
 }
 while (!isLinked);
 
-Console.WriteLine("Linked to process. Writing code"); 
-CodeInterface ci = new CodeInterface(mi);       // writes code to the main game loop that sets the world id to destiny islands every frame
-byte[] prefix = { 0x50, 0x48, 0xB8 }; // push rax ; mov rax,
-byte[] address = BitConverter.GetBytes(mi.nameToAddress("WorldID")); // WorldID
-byte[] postfix = { 0xC7, 0x00, 0x01, 0x00, 0x00, 0x00, 0x58 }; // mov [rax],1 ; pop rax
-byte[] payload = new byte[prefix.Length + address.Length + postfix.Length];
-
-int i = 0;
-foreach(byte b in prefix)
-{
-    payload[i++] = b;
-}
-foreach (byte b in address)
-{
-    payload[i++] = b;
-}
-foreach (byte b in postfix)
-{
-    payload[i++] = b;
-}
-ci.insertHook(mi.nameToAddress("MainLoopEntryOffset"), payload, 14); // inserts code at a defined symbol in offsets.csv
-
-Console.WriteLine("Success! Press Enter to Kill!"); // kills the process as it deallocates in-use memory. in a real application, don't close the dll classes until the game closes
-ci.close();
+Console.WriteLine("Linked to process. Writing code");
+DataInterface di = new DataInterface(mi);
+di.WorldID = 0;
+di.RoomID = 1;
+di.WarpID = 0;
+di.SceneID = 0;
+Console.WriteLine("Success! Press Enter to End!"); // ends the app
 mi.close();
