64b6037e3511cb36a23502a0335fdbcfcc43b6d4
synth <wavydave2000@gmail.com>
2024-07-13 23:38:50 -0400

    refactor, add latest release

---
+++ ModManagerCLI/ModManager.cs
@@ -0,0 +1,37 @@
+﻿using KHMI;
+
+Console.WriteLine("KHMI\nPress enter to attempt to link to game.");
+Console.ReadLine();
+
+ModLoader modLoader = new ModLoader(Provider.EPIC, "1.0.0.9");
+
+while (!modLoader.attach())
+{
+    Console.WriteLine("Failed to link to game. Press enter to retry, or type \"quit\" to end.");
+    string result = Console.ReadLine();
+    if (result == "quit")
+    {
+        modLoader = null;
+        break;
+    }
+}
+
+if (modLoader != null)
+{
+    Console.WriteLine("Process linked successfully. Running. Press enter to quit.");
+    while(modLoader.Runnable)
+    {
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo cki = Console.ReadKey();
+            if (cki.Key == ConsoleKey.Enter)
+            {
+                modLoader.close();
+            }
+        }
+        modLoader.runEvents();
+        Thread.Sleep(1);
+    }
+}
+
+Console.WriteLine("Exiting process.");
--- TestDLLMod/TestMod.cs
+++ TestDLLMod/TestMod.cs
@@ -8,26 +8,45 @@ namespace TestDLLMod
     {
         public TestMod(ModInterface mi) : base(mi) { }
 
-
-        public override void playerLoaded(Entity newPlayer)
+        private void setMaxHP(Entity e, int newHP)
         {
-            IntPtr entityTableStart = modInterface.memoryInterface.nameToAddress("FirstEntity");
-            IntPtr entityTableEndPtr = modInterface.memoryInterface.nameToAddress("FinalEntityPtr");
-            IntPtr entityTableEnd = (IntPtr)modInterface.memoryInterface.readLong(entityTableEndPtr);
-            EntityTable et = new EntityTable(modInterface.dataInterface, entityTableStart, entityTableEnd);
-            Entity[] entities = et.Entities;
-            Vector3[] positions = new Vector3[entities.Length];
-            Random r = new Random();
-            for(int i = 0; i < positions.Length; i++)
+            StatPage sp = e.StatPage;
+            sp.MaxHP = newHP;
+            sp.CurrentHP = Math.Min(sp.MaxHP, sp.CurrentHP);
+            PartyStatPage psp = sp.PartyStatPage;
+            if (psp != null)
             {
-                positions[i] = entities[i].Position;
-            }
-            r.Shuffle(positions);
-            for(int i = 0; i < entities.Length; i++)
-            {
-                entities[i].Position = positions[i];
+                psp.MaxHP = (byte)sp.MaxHP;
             }
         }
 
+        public override void onHPChange(Entity target)
+        {
+            if (target.IsPartyMember)
+            {
+                int newHP = (target.StatPage.MaxHP + target.StatPage.CurrentHP) / 2;
+                setMaxHP(target, newHP);
+            }
+            else
+            {
+                Entity player = Entity.getPlayer(modInterface.dataInterface);
+                if (player != null)
+                {
+                    setMaxHP(player, player.StatPage.MaxHP + 1);
+                }
+            }
+        }
+
+        public override void onEntityDeath(Entity deceased)
+        {
+            if (deceased.IsPartyMember)
+            {
+                if (!deceased.IsPlayer)
+                {
+                    Entity player = Entity.getPlayer(modInterface.dataInterface);
+                    setMaxHP(player, player.StatPage.MaxHP * 2 / 3);
+                }
+            }
+        }
     }
 }
