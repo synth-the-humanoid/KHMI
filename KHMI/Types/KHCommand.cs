c494da424a33afabe8699152a2490c81f5e49a07
synth <wavydave2000@gmail.com>
2024-08-02 16:23:46 -0400

    bugfix and new release

--- KHMI/Types/KHCommand.cs
+++ KHMI/Types/KHCommand.cs
@@ -4,11 +4,11 @@
     {
         public KHCommand(DataInterface di, IntPtr address) : base(di, address) { }
 
-        public KHCommand FromID(int id)
+        public static KHCommand FromID(DataInterface di, int id)
         {
-            IntPtr baseArrayPtr = memoryInterface.nameToAddress("CommandBasePtr");
-            IntPtr baseAddress = (IntPtr)memoryInterface.readLong(baseArrayPtr);
-            return new KHCommand(dataInterface, baseAddress + id * 0x10);
+            IntPtr baseArrayPtr = di.modInterface.memoryInterface.nameToAddress("CommandBasePtr");
+            IntPtr baseAddress = (IntPtr)di.modInterface.memoryInterface.readLong(baseArrayPtr);
+            return new KHCommand(di.modInterface.dataInterface, baseAddress + id * 0x10);
         }
 
         public short ActionID
