26e4e023e8a6b37ff39048a0750c091fbfb24255
synth <wavydave2000@gmail.com>
2024-08-02 16:15:59 -0400

    add khcommand, update release

---
+++ KHMI/Types/KHCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace KHMI.Types
+{
+    public class KHCommand : KHMIType
+    {
+        public KHCommand(DataInterface di, IntPtr address) : base(di, address) { }
+
+        public KHCommand FromID(int id)
+        {
+            IntPtr baseArrayPtr = memoryInterface.nameToAddress("CommandBasePtr");
+            IntPtr baseAddress = (IntPtr)memoryInterface.readLong(baseArrayPtr);
+            return new KHCommand(dataInterface, baseAddress + id * 0x10);
+        }
+
+        public short ActionID
+        {
+            get
+            {
+                return memoryInterface.readShort(address + 0x4);
+            }
+            set
+            {
+                memoryInterface.writeShort(address + 0x4, value);
+            }
+        }
+
+        public KHAction Action
+        {
+            get
+            {
+                return KHAction.FromID(dataInterface, ActionID);
+            }
+        }
+    }
+}
